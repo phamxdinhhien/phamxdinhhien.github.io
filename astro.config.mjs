d6892503b6465341df2b7f61e516180f094789b6
phamdinhhien <pham.dinhhien@bsci.com>
2026-01-11 11:18:41 +0700

    feat: add user name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://phamxdinhhien.github.io',
   base: '/hien_portfolio',
   output: 'static',
 });
